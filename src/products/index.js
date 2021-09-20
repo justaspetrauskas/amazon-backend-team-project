f27b196b24a296f2c5c6882ba819df0975046fef
Zukhriddin <zuxr.akhmedov@gmail.com>
2021-09-20 18:15:27 +0500

    validation file

--- src/products/index.js
+++ src/products/index.js
@@ -8,6 +8,8 @@ import path, { dirname } from "path";
 
 import { fileURLToPath } from "url";
 
+import { checkProductSchema, checkValidationResult } from "./validation.js";
+
 const _filename = fileURLToPath(import.meta.url);
 
 const _dirname = dirname(_filename);
@@ -32,179 +34,90 @@ productsRouter.get("/", async (req, res, next) => {
 //to get a single product
 productsRouter.get("/:id", async (req, res, next) => {
     try {
+        const fileAsBuffer = fs.readFileSync(productsFilePath);
+        const fileAsString = fileAsBuffer.toString();
+        fileAsJson = JSON.parse(fileAsString);
+        const product = fileAsJson.find((product) => product.id === req.params.id);
+        if (!product) {
+            res
+                .status(404)
+                .send({ message: `Product with ${req.params.id} is not found` });
+        }
+        res.send(product);
+    } catch (error) {
+        res.send(500).send({ message: error.message });
+    }
+});
+
+//to post a product
+productsRouter.post("/", checkProductSchema, checkValidationResult, async (req, res, next) => {
+    try {
+        const product = {
+            id: uniqid(),
+            ...req.body,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }
+
         const fileAsBuffer = fs.readFileSync(productsFilePath)
         const fileAsString = fileAsBuffer.toString()
-        fileAsJson = JSON.parse(fileAsString)
-        const product = fileAsJson.find((product) => product.id === req.params.id)
-        if (!product) {
-            res.status(404).send({ message: `Product with ${req.params.id} is not found` })
-        }
+        const fileAsJson = JSON.parse(fileAsString)
+        fileAsJson.push(product)
+        fs.writeFileSync(productsFilePath, JSON.stringify(fileAsJson))
         res.send(product)
     } catch (error) {
         res.send(500).send({ message: error.message })
+    }
+})
 
-        try {
-            const fileAsBuffer = fs.readFileSync(productsFilePath);
-            const fileAsString = fileAsBuffer.toString();
-            const fileAsJson = await JSON.parse(fileAsString);
-            res.status(200).send(fileAsJson);
-        } catch (error) {
-            res.send(500).send({ message: error.message });
+// to update the product
+productsRouter.put("/:id", async (req, res, next) => {
+    try {
+        const fileAsBuffer = fs.readFileSync(productsFilePath);
+        const fileAsString = fileAsBuffer.toString();
+        const fileAsJson = JSON.parse(fileAsString);
+
+        const productIndex = fileAsJson.findIndex(
+            (product) => product.id === req.params.id
+        );
+        if (!productIndex == -1) {
+            res
+                .status(404)
+                .send({ message: `product with ${req.params.id} is not found` });
         }
-    };
-
-    //to get a single product
-    productsRouter.get("/:id", async (req, res, next) => {
-        try {
-            const fileAsBuffer = fs.readFileSync(productsFilePath);
-            const fileAsString = fileAsBuffer.toString();
-            fileAsJson = JSON.parse(fileAsString);
-            const product = fileAsJson.find((product) => product.id === req.params.id);
-            if (!product) {
-                res
-                    .status(404)
-                    .send({ message: `Product with ${req.params.id} is not found` });
-            }
-            res.send(product);
-        } catch (error) {
-            res.send(500).send({ message: error.message });
-        }
-    });
-
-    //to post a product
-    productsRouter.post("/", async (req, res, next) => {
-        try {
-            const product = {
-                id: uniqid(),
-                ...req.body,
-                createdAt: new Date(),
-                updatedAt: new Date()
-            }
-
-            const fileAsBuffer = fs.readFileSync(productsFilePath)
-            const fileAsString = fileAsBuffer.toString()
-            const fileAsJson = JSON.parse(fileAsString)
-            fileAsJson.push(product)
-            fs.writeFileSync(productsFilePath, JSON.stringify(fileAsJson))
-            res.send(product)
-        } catch (error) {
-            res.send(500).send({ message: error.message })
-        }
-    })
-
-    // to update the product
-    productsRouter.put("/:id", async (req, res, next) => {
-        try {
-            const fileAsBuffer = fs.readFileSync(productsFilePath)
-            const fileAsString = fileAsBuffer.toString()
-            const fileAsJson = JSON.parse(fileAsString)
-
-            const productIndex = fileAsJson.findIndex(
-                (product) => product.id === req.params.id
-            )
-            if (!productIndex == -1) {
-                res.status(404).send({ message: `product with ${req.params.id} is not found` })
-            }
-            const previousProductData = fileAsJson[productIndex]
-            const changedProduct = {
-                ...previousProductData,
-                ...req.body,
-                updatedAt: new Date(),
-                id: req.params.id
-            }
-            fileAsJson[productIndex] = changedProduct
-            fs.writeFileSync(productsFilePath, JSON.stringify(fileAsJson))
-            res.send(changedProduct)
-        } catch (error) {
-            res.send(500).send({ message: error.message })
-        }
-    })
-
-
-    productsRouter.delete("/:id", async (req, res, next) => {
-        try {
-            const fileAsBuffer = fs.readFileSync(productsFilePath)
-            const fileAsString = fileAsBuffer.toString()
-            let fileAsJson = JSON.parse(fileAsString)
-
-            const product = fileAsJson.find((product) => product.id === req.params.id)
-            if (!product) {
-                res.status(404).send({ message: `Product with ${req.params.id} is not found!` })
-            }
-            fileAsJson = fileAsJson.filter(
-                (product) => product.id !== req.params.id
-            )
-            fs.writeFileSync(productsFilePath, JSON.stringify(fileAsJson))
-            res.status(204).send()
-        } catch (error) {
-            res.send(500).send({ message: error.message })
-            try {
-                const product = {
-                    id: uniqid(),
-                    ...req.body,
-                    createdAt: new Date(),
-                    updatedAt: new Date(),
-                };
-
-                const fileAsBuffer = fs.readFileSync(productsFilePath);
-                const fileAsString = fileAsBuffer.toString();
-                const fileAsJson = JSON.parse(fileAsString);
-                fileAsJson.push(product);
-                fs.writeFileSync(productsFilePath, JSON.stringify(fileAsJson));
-                res.send(product);
-            } catch (error) {
-                res.send(500).send({ message: error.message });
-            }
+        const previousProductData = fileAsJson[productIndex];
+        const changedProduct = {
+            ...previousProductData,
+            ...req.body,
+            updatedAt: new Date(),
+            id: req.params.id,
         };
+        fileAsJson[productIndex] = changedProduct;
+        fs.writeFileSync(productsFilePath, JSON.stringify(fileAsJson));
+        res.send(changedProduct);
+    } catch (error) {
+        res.send(500).send({ message: error.message });
+    }
+});
 
-        // to update the product
-        productsRouter.put("/:id", async (req, res, next) => {
-            try {
-                const fileAsBuffer = fs.readFileSync(productsFilePath);
-                const fileAsString = fileAsBuffer.toString();
-                const fileAsJson = JSON.parse(fileAsString);
+productsRouter.delete("/:id", async (req, res, next) => {
+    try {
+        const fileAsBuffer = fs.readFileSync(productsFilePath);
+        const fileAsString = fileAsBuffer.toString();
+        let fileAsJson = JSON.parse(fileAsString);
 
-                const productIndex = fileAsJson.findIndex(
-                    (product) => product.id === req.params.id
-                );
-                if (!productIndex == -1) {
-                    res
-                        .status(404)
-                        .send({ message: `product with ${req.params.id} is not found` });
-                }
-                const previousProductData = fileAsJson[productIndex];
-                const changedProduct = {
-                    ...previousProductData,
-                    ...req.body,
-                    updatedAt: new Date(),
-                    id: req.params.id,
-                };
-                fileAsJson[productIndex] = changedProduct;
-                fs.writeFileSync(productsFilePath, JSON.stringify(fileAsJson));
-                res.send(changedProduct);
-            } catch (error) {
-                res.send(500).send({ message: error.message });
-            }
-        });
+        const product = fileAsJson.find((product) => product.id === req.params.id);
+        if (!product) {
+            res
+                .status(404)
+                .send({ message: `Product with ${req.params.id} is not found!` });
+        }
+        fileAsJson = fileAsJson.filter((product) => product.id !== req.params.id);
+        fs.writeFileSync(productsFilePath, JSON.stringify(fileAsJson));
+        res.status(204).send();
+    } catch (error) {
+        res.send(500).send({ message: error.message });
+    }
+});
 
-        productsRouter.delete("/:id", async (req, res, next) => {
-            try {
-                const fileAsBuffer = fs.readFileSync(productsFilePath);
-                const fileAsString = fileAsBuffer.toString();
-                let fileAsJson = JSON.parse(fileAsString);
-
-                const product = fileAsJson.find((product) => product.id === req.params.id);
-                if (!product) {
-                    res
-                        .status(404)
-                        .send({ message: `Product with ${req.params.id} is not found!` });
-                }
-                fileAsJson = fileAsJson.filter((product) => product.id !== req.params.id);
-                fs.writeFileSync(productsFilePath, JSON.stringify(fileAsJson));
-                res.status(204).send();
-            } catch (error) {
-                res.send(500).send({ message: error.message });
-            }
-        });
-
-        export default productsRouter
+export default productsRouter
--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ server.use(cors());
 server.use(express.json());
 
 server.use("/products", productsRouter);
-server.listen(PORT, () => console.log("The server running on port"));
+server.listen(PORT, () => console.log("The server running on port:", PORT));
 
 server.on("error", (error) =>
     console.log(`Server is not running due to: ${error}`)
